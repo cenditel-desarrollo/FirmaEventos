8ef5d2c25a2e2800079b046befbdf2e2b70ba95f
rudmanmrrod <rudman22@hotmail.com>
2017-11-20 11:12:48 -0400

    Agregados modelos al admin de django

--- participantes/admin.py
+++ participantes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Participante, ParticipanteEvento
 
-# Register your models here.
+admin.site.register(Participante)
+admin.site.register(ParticipanteEvento)
