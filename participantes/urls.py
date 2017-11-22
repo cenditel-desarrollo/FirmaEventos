e91566ebb26ca0e006aaae2e9faf43f07d28f1d4
Leonel Hernandez <leonelphm@gmail.com>
2017-11-22 06:18:22 -0400

    Agregada url en app participantes

--- participantes/urls.py
+++ participantes/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     url(r'^search-participante/(?P<pk>\d+)/(?P<pasaporte>.+)$', ParticipanteEventoSearch.as_view(), name='search_participante'),
     url(r'^search-participante/$', ParticipanteEventoSearch.as_view(), name='search_participante_nid'),
+    url(r'^update-participante-evento/$', AjaxParticipanteFirmaEvento.as_view(), name='update_participante_evento'),
 ]
